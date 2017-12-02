b3d74eeffeef1bde958becd4aae6dfbca311ee19
SigurdurAxel <posno123@gmail.com>
2017-12-02 02:54:29 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 import os
 from bottle import *
 
-@route('/')
-def index():
-    return "Hallo heimur"
+@route('/static/<skraarnafn>')
+def static_skrar(skraarnafn):
+    return static_file(skraarnafn, root='./static')
 
-run(host='0.0.0.0', port=os.environ.get('PORT'))
+@route("/")
+def index():
+    return template("forsida.tpl")
+
+kerra = []
+@route("/add/<item>")
+def add(item):
+        kerra.append(item)
+        redirect("/")
+
+run(host='localhost', port=8080)
